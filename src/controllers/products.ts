d5bc04fe0ed40fad6d762a6e90ee171209d295fe
staskarpenko1 <karpenko.stas@gmail.com>
2024-11-07 23:12:08 -0500

    finish

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -10,18 +10,21 @@ const getAllProductsStatic = async ( req: Request , res: Response ) => {
 }
 
 const getAllProducts = async ( req: Request , res: Response ) => {
-    const { featured, company, name, sort, fields } = req.query as { 
+    const { featured, company, name, sort, fields, numericFilters } = req.query as { 
         featured?: string, 
-        company: string, 
-        name: string, 
-        sort: string,
-        fields: string,
+        company?: string, 
+        name?: string, 
+        sort?: string,
+        fields?: string,
+        numericFilters?: string
     };
 
     const queryObject: { 
         featured?: boolean,
         company?: string, 
-        name?: string | { $regex: string; $options: string }
+        name?: string | { $regex: string; $options: string },
+        [key: string]: any
+
 
      } = {} 
 
@@ -33,6 +36,28 @@ const getAllProducts = async ( req: Request , res: Response ) => {
     }
     if (name){
         queryObject.name = { $regex: name, $options: 'i'}
+    }
+    if (numericFilters){
+       const operatorMap : { [key: string]: string} = {
+        '>':'$gt',
+        '>=':'$gte',
+        '=':'$eq',
+        '<':'$lt',
+        '<=':'$lte',
+       } 
+       const regex = /\b(<|>|>=|<=|=)\b/g
+
+       let filters: string | void = numericFilters.replace(
+        regex, 
+        (match) => `-${operatorMap[match]}-`);
+
+        const options = [ 'price', 'rating' ];
+        filters = filters.split(',').forEach((item: string) => {
+            const [field, operator, value] : Array<string> = item.split('-');
+            if (options.includes(field)){
+                queryObject[field] = { [operator]: Number(value) }
+            }
+        })
     }
 
     let result = Products.find(queryObject);
@@ -55,7 +80,7 @@ const getAllProducts = async ( req: Request , res: Response ) => {
 
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
-    const skip = (page -1 ) * limit
+    const skip = (page-1 ) * limit
 
     result = result.skip(skip).limit(limit)
 
